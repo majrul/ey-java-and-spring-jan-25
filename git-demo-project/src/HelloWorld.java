bcea0e943e070557cb0b04b2706a7a03da988972
majrul <majrul@localhost>
2025-01-25 12:02:24 +0530

    small change

--- git-demo-project/src/HelloWorld.java
+++ git-demo-project/src/HelloWorld.java
@@ -1,7 +1,10 @@
+import java.time.LocalDate;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		LocalDate d = LocalDate.now();
+		System.out.println(d);
 	}
 }
