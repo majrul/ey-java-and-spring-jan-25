399be53e2b91bd092b6cd984076092122017ade9
majrul <majrul@localhost>
2025-01-28 19:51:57 +0530

    updated code

--- spring-boot-proj/src/main/java/com/training/component/HelloWorld.java
+++ spring-boot-proj/src/main/java/com/training/component/HelloWorld.java
@@ -1,11 +1,28 @@
 package com.training.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
 
 @Component
 class HelloWorld {
 	
-	public HelloWorld() {
-		System.out.println("HelloWorld()");
+	//@Value("${app.msg}")
+	private final String msg;
+	
+	/*public HelloWorld() {
+		System.out.println(msg);
+		//System.out.println("HelloWorld()");
+	}*/
+	
+	public HelloWorld(@Value("${app.msg}") String msg) {
+		this.msg = msg;
+		System.out.println(msg);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println(msg);
 	}
 }
