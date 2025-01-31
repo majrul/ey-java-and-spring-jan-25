e64d83a31cce911fbcee34ac7b0a65911717705a
majrul <majrul@localhost>
2025-01-31 19:41:20 +0530

    updated code get operation

--- spring-boot-app/src/main/java/com/training/controller/OrderController.java
+++ spring-boot-app/src/main/java/com/training/controller/OrderController.java
@@ -1,16 +1,23 @@
 package com.training.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.training.dto.OrderDetails;
 import com.training.dto.OrderStatus;
+import com.training.entity.Order;
 import com.training.exception.OrderException;
 import com.training.service.OrderService;
+
 
 @RestController
 public class OrderController {
@@ -78,4 +85,10 @@ public class OrderController {
 		return new ResponseEntity<OrderStatus>(orderStatus, HttpStatus.OK);
 	}
 
+	// http://localhost:8080/orderDetails/john@gmail.com
+	@GetMapping("/orderDetails/{email}")
+	public List<Order> getOrders(@PathVariable String email) {
+		return orderService.getOrders(email);
+	}
+	
 }
